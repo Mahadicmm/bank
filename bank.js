34d46af0e9b9db726b0e6239b47342a4e93d40fb
Mahadicmm <113575352+Mahadicmm@users.noreply.github.com>
2022-11-27 22:00:51 +0600

    Create bank.js
    email update

--- bank.js
+++ bank.js
@@ -2,7 +2,7 @@ let loginEmail = document.querySelector('.login-email');
 let loginPassword = document.querySelector('.login-password');
 let submit_button = document.querySelector('.submit-btn').addEventListener('click', function(){
    
-    if(loginEmail.value == 'mahadi1971@gmail.com' && loginPassword.value =='123456'){
+    if(loginEmail.value == 'mahadi@gmail.com' && loginPassword.value =='123456'){
         window.location.href = "amount.html";
     }
     else{
